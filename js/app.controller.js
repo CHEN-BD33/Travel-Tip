1ca9b40c15765a1023c296308c33ab807dff93ed
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-01-24 09:17:23 +0200

    Add grouping by last Updated and  chart

--- js/app.controller.js
+++ js/app.controller.js
@@ -274,6 +274,9 @@ function renderLocStats() {
     locService.getLocCountByRateMap().then(stats => {
         handleStats(stats, 'loc-stats-rate')
     })
+    locService.getLocCountByUpdatedMap().then(stats => {
+        handleStats(stats, 'loc-stats-updated')
+    })
 }
 
 function handleStats(stats, selector) {
